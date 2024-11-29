6af15f37b1226de34734bc1b0bf58b151e4a59d0
Ahmedalnagam <104149973+Ahmedalnagam@users.noreply.github.com>
2024-11-29 19:06:53 +0200

    fixed herf link don't add path it's not path it's url

--- back-end/routes/static-files.mjs
+++ back-end/routes/static-files.mjs
@@ -12,8 +12,8 @@ const __dirname = path.dirname(__filename);
 const router = express.Router();
 
 //serving static files like js,css and images
-router.use('/javascript', express.static(path.join(__dirname, '../../Frontend/JAVASCRIPT')));
-router.use('/CSS', express.static(path.join(__dirname, '../../Frontend/CSS')));
-router.use('/pictures', express.static(path.join(__dirname, '../../Frontend/pictures')));
+router.use('/javascript', express.static(path.join(__dirname, '../../front-end/javascript')));
+router.use('/CSS', express.static(path.join(__dirname, '../../front-end/css')));
+router.use('/pictures', express.static(path.join(__dirname, '../../front-end/pictures')));
 
 export default router;
